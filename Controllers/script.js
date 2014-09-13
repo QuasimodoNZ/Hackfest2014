b535c954ece8235b88c396549918d3da90623023
SamuSan <sambminns@gmail.com>
2014-09-13 15:45:13 +1200

    GET into controller, index updated

--- Controllers/script.js
+++ Controllers/script.js
@@ -3,9 +3,18 @@
  */
 var findaHomeApp = angular.module('findaHomeApp', []);
 
-findaHomeApp.controller('mainController', function($scope){
+findaHomeApp.controller('mainController', function($scope, $http){
 
     $scope.someWord = "SomESHITs";
+    $scope.homes =
+        $http({method:'GET', url:'/https://api.trademe.co.nz/v1/Search/Property/Residential.json?category=3399&page=1&rows=240&return_metadata=true'}).
+            success(function(data,status,headers,config){
+
+            }).
+            error(function(data, status, headers, config){
+
+            });
+
 
 
 });
