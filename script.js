a12b7ae9ab45a73f9180a33ced2905a0647d1cad
SamuSan <sambminns@gmail.com>
2014-09-13 15:19:44 +1200

    Angular working, superflous

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by samminns on 13/09/14.
+ */
+var findaHomeApp = angular.module('findaHomeApp', []);
+
+findaHomeApp.controller('mainController', function($scope){
+
+    $scope.someWord = "SomESHITs";
+
+
+});
