3eb96ef3dbaf155d27baee1b8792763c16364bf0
Benjamin Riddell <benjamin.riddell@hotmail.com>
2014-09-14 15:25:23 +1200

    NO ERRORS WITH THE HEATMAP!!!

--- Controllers/script.js
+++ Controllers/script.js
@@ -5,7 +5,7 @@ var findaHomeApp = angular.module('findaHomeApp', []);
 var bands = 5;
 var auth = 'oauth_consumer_key=5815646823D13FA7222C0797A2749E60&oauth_token=DA88138F569C33AD938CA41753487034&oauth_signature_method=PLAINTEXT&oauth_signature=8A6E9A02BD6316ACA7CB3F2D57C28609%26F0676D085E1F584D5992FBE42D6546A3';
 findaHomeApp.controller('mainController', function ($scope, $http, $q) {
-
+var map;
     function setUpMap() {
         console.log("Setting up map");
         var mapOptions = {
@@ -14,16 +14,24 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
+        console.log(map);
+        console.log(document.getElementById('map-canvas'));
     }
 
     //google.maps.event.addDomListener(window, 'load', initialize);
     setUpMap();
     function applyHeatmap(homeBands) {
+        map = document.getElementById('map-canvas');
+        if(!map)
+            map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        console.log("Applying heatmap");
         // Where homeBands is an array of of arrays of google.maps.LatLng(float, float)
         for (var i = 0; i < homeBands.length; i++) {
-            //debugger;
             var homes = [];
 
+            console.log("Making pointArray for the "+i+"th band");
             for(var j = 0; j < homeBands[i].length; j++){
                 if(!homeBands[i][j].hasOwnProperty('GeographicLocation'))
                     continue;
@@ -34,16 +42,29 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
                 homes.push(new google.maps.LatLng(lat, lng));
             }
 
+            console.log("Applying the pointArray for the "+i+"th band to the map");
             var pointArray = new google.maps.MVCArray(homes);
-            if(pointArray.length==0)
+            console.log(pointArray);
+
+            if(!pointArray || pointArray.length==0)
                 continue;
 
+            console.log("Creating the HeatmapLayer Object");
             var heatmap = new google.maps.visualization.HeatmapLayer({
                 data: pointArray
             });
 
+            console.log("Setting the colour");
             heatmap.set('gradient', getColour(i, homeBands.length));
-            heatmap.setMap($scope.map);
+            console.log("Setting the map-canvas thingy");
+            try{
+                heatmap.setMap(map);
+                console.log("Succesfully set the map-canvas thingy");
+            }catch(err){
+                console.log(err.message);
+            }
+            //if(i==4)
+              //  debugger;
         }
     }
 
@@ -59,11 +80,11 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
     $http({method: 'GET', url: 'https://api.trademe.co.nz/v1/Search/Property/Residential.json?category=3399&page=1&rows=500&region=15&return_metadata=true&' + auth}).
         success(function (data, status, headers, config) {
             getAllPages(data).then(function (things) {
-//                debugger;
                 var houseList = spitToList(things);
                 console.log(houseList);
                 houseList.sort(priceSort);
                 var housesByBand = splitToBand(houseList);
+                console.log("Calling the applyHeatmap function");
                 applyHeatmap(housesByBand);
             });
 
@@ -106,7 +127,6 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
             num +=inc;
             allBandsArr.push(band);
         }
-        debugger;
         return allBandsArr;
     }
 
