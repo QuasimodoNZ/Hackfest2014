5ed02f37d69ec04413581862c62814f915536512
SamuSan <sambminns@gmail.com>
2014-09-14 14:04:15 +1200

    Banding for properties

--- Controllers/script.js
+++ Controllers/script.js
@@ -2,19 +2,20 @@
  * Created by samminns on 13/09/14.
  */
 var findaHomeApp = angular.module('findaHomeApp', []);
-var bands = 10;
+var bands = 5;
 var auth = 'oauth_consumer_key=5815646823D13FA7222C0797A2749E60&oauth_token=DA88138F569C33AD938CA41753487034&oauth_signature_method=PLAINTEXT&oauth_signature=8A6E9A02BD6316ACA7CB3F2D57C28609%26F0676D085E1F584D5992FBE42D6546A3';
 findaHomeApp.controller('mainController', function ($scope, $http, $q) {
 
-    function setUpMap(){
+    function setUpMap() {
         console.log("Setting up map");
         var mapOptions = {
-          center: { lat: -34.397, lng: 150.644},
-          zoom: 8
+            center: { lat: -34.397, lng: 150.644},
+            zoom: 8
         };
         var map = new google.maps.Map(document.getElementById('map-canvas'),
             mapOptions);
     }
+
     //google.maps.event.addDomListener(window, 'load', initialize);
     setUpMap();
     function applyHeatmap(homes) {
@@ -24,27 +25,28 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
             var individualProperties = homes[i][0];
             debugger;
 
-          var pointArray = new google.maps.MVCArray(homes[i]);
-          if(pointArray.length==0)
-            continue;
+            var pointArray = new google.maps.MVCArray(homes[i]);
+            if (pointArray.length == 0)
+                continue;
 
-          var heatmap = new google.maps.visualization.HeatmapLayer({
-            data: pointArray
-          });
+            var heatmap = new google.maps.visualization.HeatmapLayer({
+                data: pointArray
+            });
 
-          heatmap.set('gradient', getColour(i, homes.length));
-          heatmap.setMap(map);
-        };
-      }
-
-      function getColour(index, length){
-        if(index < length/2){
-          return ('rgba(255, ' + (255 * index / length) +', 0, 1)');
+            heatmap.set('gradient', getColour(i, homes.length));
+            heatmap.setMap(map);
         }
-        else{
-          return 'rgba(' + (255 * index / length) + ', 255, 1)';
+        ;
+    }
+
+    function getColour(index, length) {
+        if (index < length / 2) {
+            return ('rgba(255, ' + (255 * index / length) + ', 0, 1)');
         }
-      }
+        else {
+            return 'rgba(' + (255 * index / length) + ', 255, 1)';
+        }
+    }
 
     $http({method: 'GET', url: 'https://api.trademe.co.nz/v1/Search/Property/Residential.json?category=3399&page=1&rows=500&region=15&return_metadata=true&' + auth}).
         success(function (data, status, headers, config) {
@@ -77,7 +79,7 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
         var splitList = new Array();
         arr.forEach(function (coll) {
             coll.data.List.forEach(function (house) {
-                if (house.RateableValue != undefined && house.RateableValue > 100000) {
+                if (house.RateableValue && parseInt(house.RateableValue, 10) > 100000) {
                     splitList.push(house);
                 }
             })
@@ -86,32 +88,22 @@ findaHomeApp.controller('mainController', function ($scope, $http, $q) {
     }
 
     function splitToBand(col) {
-        var minRV = col[0].RateableValue;
-        var maxRV = col[col.length - 1].RateableValue;
-        console.log(maxRV);
-        console.log(minRV);
-        var rangeRV = maxRV - minRV;
-        var inc = rangeRV / bands;
-        console.log(inc);
         var allBandsArr = new Array();
-        for (var i = 1; i <= bands; i++) {
-            var bandArr = new Array();
-            bandArr.push(col.filter(function (house) {
-                return ( ( house.RateableValue > (i-1 * inc))&&(( house.RateableValue < (i * inc))) );
-            }))
-            allBandsArr.push(bandArr);
+        var inc = col.length /bands;
+        var num  =  inc;
+        var idx =0;
+        for(var i =0; i < bands ; i++){
+            var band = col.slice(idx, num);
+            idx = num;
+            num +=inc;
+            allBandsArr.push(band);
         }
+        debugger;
         return allBandsArr;
     }
 
 });
 function priceSort(a, b) {
     console.log("sorting fings");
-    if (a.RateableValue < b.RateableValue) {
-        return -1;
-    }
-    else if (a.RateableValue > b.RateableValue) {
-        return 1;
-    }
-    return 0;
+    return a.RateableValue - b.RateableValue;
 }
